5b9d0ff23d8d82742f24df54d84dac9da3549a9c
Anas Rasyid <anasrasyid333@gmail.com>
2021-06-20 13:42:51 +0700

    Web API Source code

---
+++ Web API/sister-versioning-server/Controllers/versioningController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using sister_versioning_server.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace sister_versioning_server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class versioningController : ControllerBase
+    {
+        public static string currentVersion = "0.0.1";
+
+        public static List<Versioning> versionings = new List<Versioning>()
+        {
+            new Versioning(currentVersion, "Insert Link Here", "Insert Desc Here")
+        };
+
+        // GET: api/<VersioningController>
+        /// <summary>
+        /// Get Lastest Version
+        /// </summary>
+        /// <returns>lastest version</returns>
+        [HttpGet]
+        public Versioning GetLastestVersion()
+        {
+            return versionings.Count > 0 ? versionings[versionings.Count - 1] : null;
+        }
+
+        // GET api/<VersioningController>/5
+        /// <summary>
+        /// Get version configuraton
+        /// </summary>
+        /// <param name="version">version to get</param>
+        /// <returns>selected version</returns>
+        [HttpGet("{version}")]
+        public Versioning GetSelectedVersion(string version)
+        {
+            return versionings.Find(ver => ver.Version == version);
+        }
+
+        // POST api/<VersioningController>
+        /// <summary>
+        /// Post version configuraton
+        /// </summary>
+        /// <param name="versioning">version to post</param>
+        [HttpPost]
+        public void Post([FromBody] Versioning versioning)
+        {
+            versionings.Add(versioning);
+            currentVersion = versioning.Version;
+        }
+
+        // PUT api/<VersioningController>/5
+        /// <summary>
+        /// Update version configuraton 
+        /// </summary>
+        /// <param name="version">version to update</param>
+        /// <param name="versioning">json configuration</param>
+        [HttpPut("{version}")]                       
+        public void Put(string version, [FromBody] Versioning versioning)
+        {
+            int index = versionings.FindIndex(vers => vers.Version == version);
+            if (index != -1)
+                versionings[index] = versioning;
+
+            currentVersion = versionings[versionings.Count - 1].Version;
+        }
+
+        // DELETE api/<VersioningController>/5
+        /// <summary>
+        /// Remove version configuraton
+        /// </summary>
+        /// <param name="version">version to delete</param>
+        [HttpDelete("{version}")]
+        public void Delete(string version)
+        {
+            versionings = versionings.Where(ver => ver.Version != version).ToList();
+            currentVersion = versionings.Count > 0 ? versionings[versionings.Count - 1].Version:"-1";
+        }
+    }
+}
---
+++ Web API/sister-versioning-server/Models/Versioning.cs
@@ -0,0 +1,18 @@
+﻿namespace sister_versioning_server.Models
+{
+    public class Versioning
+    {
+        public Versioning() { }
+
+        public Versioning(string version, string link, string description)
+        {
+            Version = version;
+            Link = link;
+            Description = description;
+        }
+
+        public string Version { get; set; }
+        public string Link { get; set; }
+        public string Description { get; set; }
+    }
+}
